de3fea41508cfe623e951f826d8fc799fb1f2756
jso0003auburn <john.stephen.olson@gmail.com>
2015-07-07 12:41:00 -0400

    Google App Engine Landing Page
    Added Comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import webapp2
-
+#main landing page for the Google App engine:
+#Admin links can be used to trigger a forecast tweet or the Yes Check link will restart that process
+#These are tied to your google account so they will only work if you are logged in
 MAIN_PAGE_HTML = """\
 <html>
   <head>
@@ -11,8 +13,6 @@ MAIN_PAGE_HTML = """\
     <p>I'll give you a current update and forecast at 7:20 AM to get your day started.
     <p>After that, I'll shut up until 4:20 PM, when I'll give you an afternoon update and forecast for the drive home.
     <p>Also, if it starts raining I'll let you know.
-    <p><a class="twitter-timeline" href="https://twitter.com/olsonofab/lists/rain-check" data-widget-id="613915239151570944">Tweets from https://twitter.com/olsonofab/lists/rain-check</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     <p><b>Admin Only:</b>
     <p><a href="https://console.developers.google.com/project">Go To Console</a>
     <p><a href="https://appengine.google.com/">Go To App Engine</a>
