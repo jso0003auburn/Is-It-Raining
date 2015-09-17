7db013ae5b9acc3c967e241a64b480a355d4384b
jso0003auburn <john.stephen.olson@gmail.com>
2015-09-17 13:24:47 -0400

    Update
    Simplified the responses and removed some unnecessary files

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import webapp2
-#main landing page for the Google App engine:
-#Admin links can be used to trigger a forecast tweet or the Yes Check link will restart that process
-#These are tied to your google account so they will only work if you are logged in
+
 MAIN_PAGE_HTML = """\
 <html>
   <head>
@@ -10,14 +8,37 @@ MAIN_PAGE_HTML = """\
   <body><center>
     <h3>Is It Raining?</h3>
     <p>I'm a Twitter bot that posts weather updates.
-    <p>I'll give you a current update and forecast at 7:20 AM to get your day started.
-    <p>After that, I'll shut up until 4:20 PM, when I'll give you an afternoon update and forecast for the drive home.
-    <p>Also, if it starts raining I'll let you know.
+    <p>I live in Atlanta, but if you want me to add your city or give a suggestion just tell me: <a href="mailto:IsItRainingTwitter@gmail.com?Subject=Hey,%20rainy%20twitter" target="_top">IsItRainingTwitter@gmail.com</a>
+    <p>Current locations:
+    <p>ATA: Antarctica
+    <p>ATL: Atlanta
+    <p>ATX: Austin
+    <p>BHM: Birmingham
+    <p>BOS: Boston
+    <p>CHI: Chicago
+    <p>CHS: Charleston
+    <p>DC: D.C.
+    <p>DFW: Dallas
+    <p>HKG: Hong Kong
+    <p>HOU: Houston
+    <p>LA: Los Angeles
+    <p>LDN: London
+    <p>MIA: Miami
+    <p>RIO: Rio de Janiero
+    <p>SEA: Seattle
+    <p>SFO: San Francisco
+    <p>SYD: Sydney.
     <p><b>Admin Only:</b>
     <p><a href="https://console.developers.google.com/project">Go To Console</a>
     <p><a href="https://appengine.google.com/">Go To App Engine</a>
-    <p><a href="/tweet_forecast" target="popup" onclick="window.open('/tweet_forecast','name','width=200,height=200')">Forecast</a>
-    <p><a href="/tweet_yes" target="popup" onclick="window.open('/tweet_yes','name','width=200,height=200')">Yes Check</a>
+    <p><a href="/EST" target="_blank">EST</a>
+    <p><a href="/CST" target="_blank">CST</a>
+    <p><a href="/PST" target="_blank">PST</a>
+    <p><a href="/GMT" target="_blank">GMT</a>
+    <p><a href="/UTC" target="_blank">UTC</a>
+    <p><a href="/UTCm3" target="_blank">UTCm3</a>        
+    <p><a href="/UTC10" target="_blank">UTC10</a>
+    <p><a href="/STATS" target="_blank">STATS</a>
     <p><a href="https://www.yahoo.com/?ilc=401" target="_blank"> <img src="https://poweredby.yahoo.com/purple.png" width="134" height="29"/> </a>
     <p><img src="http://www.logicsoft.md/images/google-app-engine.png"/>
   </center></body>
--- tweet_weather.py
+++ tweet_weather.py
@@ -21,198 +21,205 @@ from urllib2 import urlopen, URLError
 
 #settings.cfg contains WOEID for city identification as well as the keys to the Twitter API
 
-config = ConfigParser.RawConfigParser()
-config.read('settings.cfg')
-WOEID = config.get('auth', 'WOEID')
 
-#	First function is designed to check the current conditions and then tweet the following
-#   Now: yes/no + random comment
-#   Later: forecasted conditions
-#   Today: Low - High
-#   Currently: Current Temp
-#
-#   Use Cron file to schedule
+
+#First function is designed to check the current conditions and then tweet the following
+#    Now: yes/no + random comment
+#    Later: forecasted conditions
+#    Today: Low - High
+#    Currently: Current Temp
+#   
+#    Use Cron to schedule
+            
+
+class EST(webapp2.RequestHandler):
+    def get(self):
+        forecast('ATL')
+        forecast('BOS')
+        forecast('DC')
+        forecast('MIA')
+
+class CST(webapp2.RequestHandler):
+    def get(self):
+        forecast('ATX')
+        forecast('BHM')
+        forecast('CHI')
+        forecast('DFW')
+        forecast('HOU')
+
+class PST(webapp2.RequestHandler):
+    def get(self):
+        forecast('LA')
+        forecast('SEA')
+        forecast('SFO')
+        
+class GMT(webapp2.RequestHandler):
+    def get(self):
+        forecast('ATA')
+        forecast('LDN')
     
-class TweetForecast(webapp2.RequestHandler):
+class UTC(webapp2.RequestHandler):
     def get(self):
-
-        try:
-            q = taskqueue.Queue('default')
-            q.purge()
-            forecastfile = urllib.urlopen("http://weather.yahooapis.com/forecastrss?w=" + WOEID + "&u=i")
-            tree = ET.parse(forecastfile)
-            root = tree.getroot()
-            channel =  root[0]
-            item = channel[12]
-            description = item[5]
-            forecast = item[7]
-            high = forecast.attrib['high']
-            low = forecast.attrib['low']
-            forecast = forecast.attrib['text']
-            currentTemp = description.attrib['temp']
-            currentText = description.attrib['text']
-            currentC = description.attrib['code']
-            currentCondition = int(currentC)
-            forecastfile.close()
-            
-            #Yahoos weather API uses certain condition codes. 
-            #Depending on the condition a comment on the weather will be generated. 
-            #comment choices can be edited in the txt files
-            #If it falls within the unique codes the script just uses Yahoos own description since theyre rare enough to not be repeated often
-            #Condition codes can be found at https://gist.github.com/bzerangue/805520
-            
-            rainCodes = [1,2,3,4,5,6,8,9,10,11,12,18,35,45,46]
-            scatteredCodes = [37,38,39,40,47]
-            fairCodes = [31,32,33,34] 
-            overcastCodes = [26,27,28]
-            blankCodes = [29,30,44]
-            snowCodes = [13,14,15,16,41,42,43,3200]
-            uniqueCodes = [17,19,20,21,22,23,24,25,36]
-
-			
-            if currentCondition in rainCodes:
-                with open('choices/yeschoices.txt') as yes_choicesf:        
-                    yes_choices = yes_choicesf.readlines()
-                    yes = random.choice(yes_choices)                
-                    yes_choicesf.close()
-                    a = yes
-                    comment = str('')
-            else:
-                with open('choices/nochoices.txt') as no_choicesf:
-                    no_choices = no_choicesf.readlines() 
-                    no = random.choice(no_choices)
-                    no_choicesf.close()
-                    a = no
-
-            if currentCondition in scatteredCodes:
-                with open('choices/scatteredchoices.txt') as scattered_choicesf:
-                    scattered_choices = scattered_choicesf.readlines() 
-                    scattered = random.choice(scattered_choices)
-                    scattered_choicesf.close()
-                    a = scattered
-                    comment = str('')
-
-            if currentCondition in fairCodes:
-                with open('choices/fairchoices.txt') as fair_choicesf:
-                    fair_choices = fair_choicesf.readlines() 
-                    fair = random.choice(fair_choices)
-                    fair_choicesf.close()
-                    comment = fair                   
-
-            if currentCondition in overcastCodes: 
-                with open('choices/overcastchoices.txt') as overcast_choicesf:
-                    overcast_choices = overcast_choicesf.readlines() 
-                    overcast = random.choice(overcast_choices)
-                    overcast_choicesf.close()
-                    comment = overcast 
+        forecast('HKG')
         
-            if currentCondition in blankCodes:
-                    comment = str('')
-        
-            if currentCondition in snowCodes:
-                with open('choices/snowchoices.txt') as snow_choicesf:
-                    snow_choices = snow_choicesf.readlines() 
-                    snow = random.choice(snow_choices)
-                    snow_choicesf.close()
-                    comment = snow
-        
-
-            if currentCondition in uniqueCodes:
-                    comment = str( ' ' + currentText + '.')
-
-            
-            #this is where the tweet is formatted and put together
-            a = a.rstrip("\r\n")                                                
-            comment = comment.rstrip("\r\n")
-            answer = ('Now: ' + a + ' ' + comment + '\n' + "Later: " + forecast + '.' + '\n' + 'Today: ' + low + '° - ' + high + '°\n' + 'Currently: ' + currentTemp + '°')
-            logging.info(answer)
-            tweet(answer)
-
-        except URLError:
-            logging.error('URLError: ' + str(sys.exc_info()[0]) + str(sys.exc_info()[1]))
-            logging.error(answer)
-
-        except IOError:
-            logging.error('IOError: ' + str(sys.exc_info()[0]) + str(sys.exc_info()[1]))
-            logging.error(answer)
-        
-        except:
-            logging.error('Unexpected error: ' + str(sys.exc_info()[0]) + str(sys.exc_info()[1]))
-            logging.error(answer)
-
-#Checks if it is raining or not and replies with a simple yes and the current temp
-#
-#Uses Google Task Queues to reload it.
-#This will ONLY tweet if it is raining.
-#If it is NOT raining, then another instance of the task is sent to the default queue
-#Polling frequency can be adjusted by changing the settings of the queue
-#
-#If it IS raining and the tweet is succesful the task queue will be purged and the operation will shut down until it is started again.
-#
-#Use Cron to start it an hour after every forecast, if it is still running from the previous day then it will just be reloaded.
-#
-#Cron only has to start it once, after that the frequency with which it checks the Yahoo Weather API will vary depending on the Queue settings
-#The Google task queue is configured by the queue.yaml file
-#Current settings limit it to 12 tasks per hour... so it checks every 5 minutes
-
-class TweetYes(webapp2.RequestHandler):
+class UTCm3(webapp2.RequestHandler):
     def get(self):
-        forecastfile = urllib.urlopen("http://weather.yahooapis.com/forecastrss?w=" + WOEID + "&u=i")
-        tree = ET.parse(forecastfile)
-        root = tree.getroot()
-        channel =  root[0]
-        item = channel[12]
-        description = item[5]
-        currentC = description.attrib['code']
-        currentCondition = int(currentC)
-        forecast = item[7]
-        high = forecast.attrib['high']
-        low = forecast.attrib['low']
-        forecast = forecast.attrib['text']
-        currentTemp = description.attrib['temp']
-        currentText = description.attrib['text']
-        forecastfile.close()
-        rainCodes = [1,2,3,4,5,6,8,9,10,11,12,18,35,45,46,47]  
-        thunderCodes = [38]  
-        if currentCondition in rainCodes:
+        forecast('RIO')  
+
+class UTC10(webapp2.RequestHandler):
+    def get(self):
+        forecast('SYD')
+
+class STATS(webapp2.RequestHandler):
+    def get(self):
+        checkup('ATA')
+        checkup('ATL')
+        checkup('ATX')
+        checkup('BHM')
+        checkup('BOS')
+        checkup('CHI')
+        checkup('DC')
+        checkup('DFW')
+        checkup('HKG')
+        checkup('HOU')
+        checkup('LA')
+        checkup('LDN')
+        checkup('MIA')
+        checkup('RIO')
+        checkup('SEA')
+        checkup('SFO')
+        checkup('SYD')
+
+def checkup(city):
+    config = ConfigParser.RawConfigParser()
+    config.read('settings.cfg')
+    myhandle = config.get(city, 'myhandle')
+    CONSUMER_KEY = config.get(city, 'CONSUMER_KEY')
+    CONSUMER_SECRET = config.get(city, 'CONSUMER_SECRET')
+    ACCESS_TOKEN = config.get(city, 'ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = config.get(city, 'ACCESS_TOKEN_SECRET')
+
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = API(auth)
+    me = api.get_user(screen_name=myhandle)
+    x = me.friends_count
+    y = me.followers_count
+    logging.critical('IsItRaining' + city + ' FOLLOWING:' + str( x ) + ' and FOLLOWERS: ' +  str( y ))
+
+
+def forecast(city):
+    config = ConfigParser.RawConfigParser()
+    config.read('settings.cfg')
+    WOEID = config.get(city, 'WOEID')
+    units = config.get(city, 'units')
+    forecastfile = urllib.urlopen("http://weather.yahooapis.com/forecastrss?w=" + WOEID + "&u=" + units)
+    tree = ET.parse(forecastfile)
+    root = tree.getroot()
+    channel =  root[0]
+    item = channel[12]
+    description = item[5]
+    forecast = item[7]
+    high = forecast.attrib['high']
+    low = forecast.attrib['low']
+    forecast = forecast.attrib['text']
+    currentTemp = description.attrib['temp']
+    currentText = description.attrib['text']
+    currentC = description.attrib['code']
+    currentCondition = int(currentC)
+    timeStamp = description.attrib['date']
+    forecastfile.close()
+    ata = 'ATA'
+
+    rainCodes = [1,2,3,4,5,6,8,9,10,11,12,18,35,37,38,39,40,47,45,46]
+    fairCodes = [31,32,33,34] 
+    overcastCodes = [26,27,28]
+    snowCodes = [13,14,15,16,41,42,43]
+    uniqueCodes = [0,1,2,3,4,5,6,7,8,10,17,18,19,20,21,22,23,24,35,37,38,39,40,44,45,46,47]
+    blankCodes = [9,11,12,25,29,30,36,3200]
+    
+    if currentCondition in rainCodes:
+        yes_choices = ['Yes','Yes','Yea','Yep','Ya','Grab an umbrella',"It's raining"]
+        a = random.choice(yes_choices)                
+
+    else:
+        no_choices = ['No','Nah','Nope','Not raining']
+        a = random.choice(no_choices)
+
+    if currentCondition in fairCodes:
+        fair_choices = [", beautiful day", ", clear day", ", nice day", ""]
+        comment = random.choice(fair_choices)
+
+    if currentCondition in overcastCodes: 
+        overcast_choices = [", gloomy", ", cloudy", ", overcast", ", grey skies", ""]
+        comment = random.choice(overcast_choices)
+
+    if currentCondition in snowCodes:
+        snow_choices = [", snowing", ", snow", ", snowfall", ", snow coming down"]
+        comment = random.choice(snow_choices)
+
+    if currentCondition in uniqueCodes:
+        uniqueChoice = str( ", " + currentText )
+        unique_choices = [uniqueChoice, ""]
+        comment = random.choice(unique_choices)
         
-            yes_choices = ['Yes.', 'Yes.', 'Yep.', "Yes...", 'Yep...', 'Ya', 'Yeah.', 'Yes.', 'Yep.', "You're going to need an umbrella.", "Yea, that's rain you're hearing"]
-            yes = random.choice(yes_choices)
-            a = str( yes + '\n' + currentTemp + '°')
-            tweet(a)  
-            logging.info(a)
-            q = taskqueue.Queue('default')
-            q.purge()
-            time.sleep(30)
+    if currentCondition in blankCodes:
+        comment = str('')
         
+    if 'Sunny' or 'Clear' in forecast:
+        forecast_choices = ["clear skies", "no clouds", "a clear sky"]
+        forecast = random.choice(forecast_choices)
+
+    if 'Cloudy' in forecast:
+        forecast_choices = ["cloudy skies", "clouds", "grey skies", "a cloudy sky"]
+        forecast = random.choice(forecast_choices)
+                        
+    if 'pm' in timeStamp:
+        timeStamp = "Tonight's low is "
+        tempHL = low
+    else:
+        timeStamp = "Today's high is "
+        tempHL = high
+
+    if ata in city:
+        timeStamp = 'later, '
+        tempHL = low
+        if currentCondition in snowCodes:
+            snow_choices = [", snow", ", snowfall", ", snow coming down"]
+            comment = random.choice(snow_choices)
+            yes_choices = ["Yes","Yep","Yea","Snowing","Ya"]
+            a = random.choice(yes_choices)            
+
         else:
-            logging.debug('Still not raining: ' + currentC + ' ' + currentText + ' ' + currentTemp)
-            taskqueue.add(url="/tweet_yes", method="GET")
-        
-        if currentCondition in thunderCodes:
-            
-			a = str( currentText + '\n' + currentTemp + '°')
-			tweet(a)  
-			logging.info(a)
-			q = taskqueue.Queue('default')
-			q.purge()
-			time.sleep(30)
+            no_choices = ['Not snowing','No','Nah']
+            a = random.choice(no_choices)
+            comment = str('')
+            pass
 
-#updates the twitter account using the codes stored in settings.cfg
-def tweet(answer):
-    CONSUMER_KEY = config.get('auth', 'CONSUMER_KEY')
-    CONSUMER_SECRET = config.get('auth', 'CONSUMER_SECRET')
-    ACCESS_TOKEN = config.get('auth', 'ACCESS_TOKEN')
-    ACCESS_TOKEN_SECRET = config.get('auth', 'ACCESS_TOKEN_SECRET')
+    a = a.rstrip("\r\n")                                                
+    comment = comment.rstrip("\r\n")
+    comment = comment.lower()
+    forecast = forecast.lower()
 
+    answer = (a + comment + '.\n' +  "Currently " + currentTemp + '°' + '.\n' + timeStamp + tempHL + "° w/ " + forecast + '.')
+    logging.info(answer)
+    CONSUMER_KEY = config.get(city, 'CONSUMER_KEY')
+    CONSUMER_SECRET = config.get(city, 'CONSUMER_SECRET')
+    ACCESS_TOKEN = config.get(city, 'ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = config.get(city, 'ACCESS_TOKEN_SECRET')
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = API(auth)
     result = api.update_status(status = answer )
 
 
-
 app = webapp2.WSGIApplication([
-    ('/tweet_forecast', TweetForecast),
-    ('/tweet_yes', TweetYes)
+    ('/EST', EST),
+    ('/CST', CST),
+    ('/PST', PST),
+    ('/GMT', GMT),
+    ('/UTC', UTC),
+    ('/UTCm3', UTCm3),
+    ('/UTC10', UTC10),
+    ('/STATS', STATS),
 ], debug=False)
